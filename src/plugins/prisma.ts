fb4085904168cf7f8f21f338ed0f094534c672e9
mohammadrahmanian <m.mohammadrahmanian@gmail.com>
2025-07-06 17:34:57 +0200

    chore: handle possible prisma plugin failures

--- src/plugins/prisma.ts
+++ src/plugins/prisma.ts
@@ -1,15 +1,33 @@
-import fp from "fastify-plugin";
-import { FastifyPluginAsync } from "fastify";
 import { PrismaClient } from "@prisma/client";
+import { FastifyPluginAsync } from "fastify";
+import fp from "fastify-plugin";
 
 export const prismaPlugin: FastifyPluginAsync = fp(async (server, options) => {
-  const prisma = new PrismaClient();
+  if (!process.env.DATABASE_URL) {
+    throw new Error("DATABASE_URL environment variable is required");
+  }
+  const prisma = new PrismaClient({
+    log:
+      process.env.NODE_ENV === "development"
+        ? ["query", "info", "warn", "error"]
+        : ["error"],
+  });
 
-  await prisma.$connect();
+  try {
+    await prisma.$connect();
+  } catch (error) {
+    server.log.error(`Error connecting to the database: ${error}`);
+    throw error;
+  }
 
   server.decorate("prisma", prisma);
 
   server.addHook("onClose", async (server) => {
-    await server.prisma.$disconnect();
+    try {
+      await server.prisma.$disconnect();
+    } catch (error) {
+      server.log.error(`Error disconnecting from the database: ${error}`);
+      throw error;
+    }
   });
 });
