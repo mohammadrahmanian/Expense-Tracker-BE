d74b13e517adcea4e70b317d57114c9d2ffcf467
mohammadrahmanian <m.mohammadrahmanian@gmail.com>
2025-07-20 21:17:13 +0200

    feat: add default categories when a user is created

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,9 +1,27 @@
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 
+import { Category } from "@prisma/client";
 import { FastifyReply, FastifyRequest } from "fastify";
 
 const saltRounds = 10;
+
+const categoriesToCreateForNewUser: Pick<
+  Category,
+  "name" | "type" | "color"
+>[] = [
+  { name: "Food", type: "EXPENSE", color: "#FF6347" },
+  { name: "Transportation", type: "EXPENSE", color: "#4682B4" },
+  { name: "Utilities", type: "EXPENSE", color: "#32CD32" },
+  { name: "Entertainment", type: "EXPENSE", color: "#FFD700" },
+  { name: "Health", type: "EXPENSE", color: "#FF4500" },
+  { name: "Household", type: "EXPENSE", color: "#8B4513" },
+  { name: "Other Expenses", type: "EXPENSE", color: "#A9A9A9" },
+  { name: "Salary", type: "INCOME", color: "#8A2BE2" },
+  { name: "Bonus", type: "INCOME", color: "#00CED1" },
+  { name: "Refund", type: "INCOME", color: "#FF69B4" },
+  { name: "Other Income", type: "INCOME", color: "#20B2AA" },
+];
 
 export const createUser = async (
   req: FastifyRequest<{ Body: { email: string; password: string } }>,
@@ -26,9 +44,14 @@ export const createUser = async (
   try {
     const hashedPassword = await bcrypt.hash(password, saltRounds);
 
-    const user = await server.prisma.user.create({
-      data: { email, password: hashedPassword },
-    });
+    const newUser = await server.prisma.user
+      .create({
+        data: { email, password: hashedPassword },
+      })
+      .catch((error) => {
+        req.log.error("Error creating user:", error);
+        throw new Error("Failed to create user");
+      });
 
     const jwtSecret = process.env.JWT_SECRET;
     if (!jwtSecret) {
@@ -37,16 +60,28 @@ export const createUser = async (
 
     const token = jwt.sign(
       {
-        userId: user.id,
+        userId: newUser.id,
       },
       jwtSecret,
       { expiresIn: "7d" }
     );
 
+    await server.prisma.category
+      .createMany({
+        data: categoriesToCreateForNewUser.map((category) => ({
+          ...category,
+          userId: newUser.id,
+        })),
+      })
+      .catch((error) => {
+        req.log.error("Error creating default categories:", error);
+        // Not throwing an error here to allow user creation even if category creation fails
+      });
+
     return reply.status(201).send({
       user: {
-        id: user.id,
-        email: user.email,
+        id: newUser.id,
+        email: newUser.email,
       },
       token,
     });
